d6f7a324a71d679f640335fc17c68545b60d6c23
Ndoh2025 <ndohwilson@yahoo.com>
2023-06-20 18:54:40 -0500

    added puppy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
+
 import './App.css'
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies) 
   return (
-    <>
-      
-    </>
+    <div className='App'>
+     {
+      puppies.map((puppy) => {
+        return <p>{puppy.name}</p>
+      })
+     } 
+    </div>
   )
 }
 
